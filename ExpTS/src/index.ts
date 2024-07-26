2787ca192b11a6c6b3b2bd833d957a17eff4081d
Lucas <lucascastro.mao@gmail.com>
2024-07-25 21:16:54 -0400

    Express 5

--- ExpTS/src/index.ts
+++ ExpTS/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import {engine} from "express-handlebars";
 
 import router from "./router/router"
 import logger from "./middleware/logger";
@@ -8,13 +9,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3333;
 
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", `${__dirname}/views`);
+
 app.use(logger("combined")); // based on requirement
 app.use("/img", express.static(`${__dirname}/../public/img`))
 app.use(router);
-
-app.get("/google", (req, res) =>{
-    res.redirect("http://google.com");
-})
 
 app.listen(PORT, () => {
   console.log(`Express app iniciada na porta ${PORT}.`);
--- ExpTS/src/router/router.ts
+++ ExpTS/src/router/router.ts
@@ -2,6 +2,27 @@ import { Router } from "express"
 
 const router = Router()
 
+router.get("/hb1", (req, res) =>{
+  res.render("hb1", {mensagem: "Alguma mensagem", layout: false});
+})
+
+router.get("/hb2", (req, res) => {
+  res.render("hb2", {
+    vencedorCaprichoso: false,
+    layout: false,
+  })
+})
+
+router.get("/hb3", (req, res) =>{
+  const profs = [
+    {name: "David Fernandes", room: 321},
+    {name: "Altigran Soares", room: 224},
+    {name: "Elaine Harada", room: 345},
+    {name: "Horácio Fernandes", room: 148},
+  ];
+  res.render("hb3", {profs, layout: false});
+});
+
 router.get("/bem-vindo/:nome", (req, res) => {
     res.send(`Seja bem-vindo(a) ${req.params.nome}`)
 });
